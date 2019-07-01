7ffd498e9a63b01b196277ee426a945637db13d6
codBogdan <1qaz2wsx*9632>
2019-07-01 19:03:02 +0300

    Exercise algorithms

--- src/main/java/org/fasttrackit/passwordvalidation/PasswordValidation.java
+++ src/main/java/org/fasttrackit/passwordvalidation/PasswordValidation.java
@@ -6,17 +6,26 @@ public class PasswordValidation {
 
     public static void main (String args[]) {
         Scanner input = new Scanner(System.in);
+        int maxAttemps = 3;
+        int attemps = 0;
         int a;
 
-        System.out.println("What is the password?");
-        a = input.nextInt();
+        while (attemps<maxAttemps) {
+            System.out.println("What is the password?");
+            a = input.nextInt();
 
-        if (a == 1234) {
-            System.out.println("Welcome!");
-        }else{
-            System.out.println("I don't know you.");
+            if (a == 1234) {
+                System.out.println("Welcome!");
+                break;
+            } else {
+                System.out.println("I don't know you.");
+            }
+            attemps++;
         }
 
+        if (attemps == maxAttemps){
+            System.out.println("\n" + "Try again later");
+        }
     }
 
 }
